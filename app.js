eaa5428d8d83522cb7aec9baa8f33ee1ec5c0020
Deekshith shetty <deekshithshetty698@gmail.com>
2022-02-27 15:38:25 +0530

    Add hashing method

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -34,7 +35,7 @@ app.get('/login', function(req, res) {
 
 app.post("/login", function(req, res) {
     const userName = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: userName}, function(err,foundOne) {
         if(err) {
             console.log(err);
@@ -53,7 +54,7 @@ app.get('/register', function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err) {
         if(err) {
